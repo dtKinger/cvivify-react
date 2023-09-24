83379108df286055f7759d34f110e9073bbba1ab
Dan King <king.daniel.thomas@gmail.com>
2023-09-25 00:28:03 +0700

    Remove Sidebar from the Print to PDF layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-
+import reactLogo from '../assets/react.svg'
 import "../styles/Header.css"
 
 function Header({children}) {
@@ -6,6 +6,14 @@ function Header({children}) {
   return (
     <>
     <header className="header output-component">
+    <div className="logo-structure">
+          <h1 className="logo-h1">
+            <span className="mini-reaction">
+              <img src={reactLogo} className="logo react mini-reaction" alt="React logo" />
+            </span>
+            Avatar
+          </h1>
+      </div>
       {children}
     </header>
     </>
