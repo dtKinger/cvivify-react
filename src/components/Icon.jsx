ddfd14e562f75537de6485754fc796ec2e73f9bd
Dan King <king.daniel.thomas@gmail.com>
2023-09-25 17:00:12 +0700

    Hide Print/preview section from print layout

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -7,7 +7,7 @@ function handlePrintClick () {
 function Icon ({classes, source, altText, text}) {
   
   return (
-    <div class="icon-container" onClick={handlePrintClick}>
+    <div className="icon-container" onClick={handlePrintClick}>
       <label htmlFor={text}>{text}</label>
       <img id={text} className={classes} src={source} alt={altText}></img>
     </div>
