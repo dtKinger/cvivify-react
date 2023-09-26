c06292a183ec49c7d1417d8d5888aaf2f30b0b78
Dan King <king.daniel.thomas@gmail.com>
2023-09-26 16:22:06 +0700

    Validate name length, and conditionally block long names over 30

--- src/components/SidebarProfile.jsx
+++ src/components/SidebarProfile.jsx
@@ -3,7 +3,11 @@ import '../styles/Sidebar.css'
 function SidebarProfile ({title, classes, data, onChange}) {
 
   const handleChange = (key, value) => {
-    onChange(key, value);
+    console.log(value.length)
+    if (value.length < 30){
+      onChange(key, value);
+    }
+    
   };
 
   return (
@@ -17,6 +21,8 @@ function SidebarProfile ({title, classes, data, onChange}) {
           type="text"
           name="professional-name"
           value={data.professional_name}
+          minrange="2"
+          maxrange="30"
           onChange={(e) => handleChange("professional_name", e.target.value)} // This controls my input
           // using the helper function handleChange which takes any prop as a key, then sets value
         />
@@ -26,8 +32,15 @@ function SidebarProfile ({title, classes, data, onChange}) {
           type="text"
           name="email"
           value={data.email_address}
-          onChange={(e) => handleChange("email_address", e.target.value)} // This controls my input
-          // using the helper function handleChange which takes any prop as a key, then sets value
+          onChange={(e) => handleChange("email_address", e.target.value)}
+        />
+        <label className="form-label form-label__phone" htmlFor="phone">Phone: </label>
+        <input
+          id="phone"
+          type="text"
+          name="phone"
+          value={data.phone_number}
+          onChange={(e) => handleChange("phone_number", e.target.value)}
         />
       </div>
     </section>
