0fd85c6e4177a3e05aa59ba252fb1deac423f440
Dan King <king.daniel.thomas@gmail.com>
2023-09-27 13:48:13 +0700

    Create a CSS ruler up to 8.5 inches

--- src/components/OutputArea.jsx
+++ src/components/OutputArea.jsx
@@ -2,9 +2,60 @@ import '../styles/OutputArea.css'
 
 function OutputArea ({children}) {
   return (
+    <>
     <div className="output-container">
+    <div className="scale-image">
+        <div class='ruler'>
+          <div class='in'>
+            <div class='qi'></div>
+            <div class='hi'></div>
+            <div class='tqi'></div>
+          </div>
+          <div class='in'>
+            <div class='qi'></div>
+            <div class='hi'></div>
+            <div class='tqi'></div>
+          </div>
+          <div class='in'>
+            <div class='qi'></div>
+            <div class='hi'></div>
+            <div class='tqi'></div>
+          </div>
+          <div class='in'>
+            <div class='qi'></div>
+            <div class='hi'></div>
+            <div class='tqi'></div>
+          </div>
+          <div class='in'>
+            <div class='qi'></div>
+            <div class='hi'></div>
+            <div class='tqi'></div>
+          </div>
+          <div class='in'>
+            <div class='qi'></div>
+            <div class='hi'></div>
+            <div class='tqi'></div>
+          </div>
+          <div class='in'>
+            <div class='qi'></div>
+            <div class='hi'></div>
+            <div class='tqi'></div>
+          </div>
+          <div class='in'>
+            <div class='qi'></div>
+            <div class='hi'></div>
+            <div class='qi'></div>
+          </div>
+          <div class='in'>
+            <div class='qi'></div>
+            <div class='hi'></div>
+          </div>
+        </div>
+      </div>
       {children}
     </div>
+    
+    </>
   )
 }
 
