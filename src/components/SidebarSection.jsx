fd9f630c9cd2fac21d6f1d7d479fe6f4411303c2
Dan King <king.daniel.thomas@gmail.com>
2023-09-27 21:42:44 +0700

    labels, inputs, and styles for SidebarSection

--- src/components/SidebarSection.jsx
+++ src/components/SidebarSection.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
 import '../styles/Sidebar.css'
 
-function InputContainer ({keyCounter}) {
+function InputContainer (keyCounter, otherCounter, title) {
+
+  console.log(otherCounter)
+  let section ;
+  let jobId = `job-title-${otherCounter}`
+  let startId = `start-date-${otherCounter}`
+  let endId = `end-date-${otherCounter}`
 
   return (
     <div key={keyCounter} className="inputs-container">
-      <button className="btn delete-btn" onClick={handleRemoveInput}>Remove Section</button>
-      <textarea rows="10" className="textarea" placeholder="Your info here..."></textarea>
+      <button className="btn delete-btn" onClick={handleRemoveInput}>Remove</button>
+      <label className="form-label form-label__job-title" htmlFor="job-title">Your Role: </label>
+      <input className="form-input form-input__job-title" id="job-title" name="job-title" type="text"></input>
+      <label className="form-label form-label__start-date" htmlFor="start-date">From:</label>
+      <input className="form-input form-input__start-date" id="start-date" name="start-date" type="date" />
+      <label className="form-label form-label__end-date" htmlFor="end-date">To:</label>
+      <input className="form-input form-input__end-date" id="end-date" name="end-date" type="date" />
+      <label className="form-label form-label__textarea" htmlFor="textarea">Describe your role:</label>
+      <textarea id="textarea" rows="10" className="textarea" placeholder="Your info here..."></textarea>
     </div>
   )
 }
@@ -18,14 +31,19 @@ function handleRemoveInput (e) {
 function SidebarSection ({title, classes}) {
   
   const [keyCounter, setKeyCounter] = useState(0);
+  const [otherCounter, setOtherCounter] = useState(0);
   const [inputContainerList, setInputContainerList] = useState([<InputContainer key={keyCounter}/>]);
 
   const handleAddBtnClick = () => {
     setKeyCounter(keyCounter + 1);
+    setOtherCounter(otherCounter + 1);
     setInputContainerList(inputContainerList.concat(<InputContainer key={keyCounter + 1} />));
+    
+    return otherCounter;
   };
 
   return (
+    otherCounter,
     <section className={classes}>
       <h2 className="title">{title}</h2>
       {inputContainerList}
