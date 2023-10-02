1eeec6fcee276b76aee2a0323210a8d6ee39c8d7
Dan King <king.daniel.thomas@gmail.com>
2023-10-02 17:23:49 +0700

    Add section headers

--- src/components/OutputExperience.jsx
+++ src/components/OutputExperience.jsx
@@ -7,14 +7,14 @@ function OutputExperience({ experienceNodes }) {
     <div className="output-component">
       {experienceNodes.map((node, index) => (
         <div key={index} className="experience-node">
-          <h2 className="company-name">{node.company_name}</h2>
-          <h3 className="job-title">{node.job_title}</h3>
+          <h2 className="company-name"><span className="labels">Employer: </span>{node.company_name}</h2>
+          <h3 className="job-title"><span className="labels">Role: </span>{node.job_title}</h3>
           <div className="job-dates">
-            <span className="start-date">{node.starting_date} -</span>
-            <span className="end-date"> {node.worked_until}</span>
+            <span className="start-date"><span className="labels">From: </span>{node.starting_date} -</span>
+            <span className="end-date"><span className="labels">Until: </span>{node.worked_until}</span>
           </div>
           <div className="profile-header__mission">
-            <p className="job-description">{node.job_description}</p>
+            <p className="job-description"><span className="labels">Description: </span>{node.job_description}</p>
           </div>
         </div>
       ))}
